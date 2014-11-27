85db2a4fb78485e0920e2d4e44b5865ab09c74b1
tysonseaborn <tysonseaborn@gmail.com>
2014-11-27 17:41:47 -0500

    #FUCKSNOOKIINDABUTT

--- app/src/main/java/com/example/tyson/transguardserver/TransGuardServer.java
+++ app/src/main/java/com/example/tyson/transguardserver/TransGuardServer.java
@@ -2,7 +2,9 @@ package com.example.tyson.transguardserver;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,71 +23,80 @@ import java.net.URL;
 public class TransGuardServer extends Activity {
 
     String apiKey = "AIzaSyBWfKLPBvX8P4tm2sI4bKiT4LA2XUyejp4";
+    //String apiKey = "AIzaSyAnQRrzN8JVNqCSG6NKNJPLcLEPCSqLdyw";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trans_guard_server);
     }
 
-    public static void post(String apiKey, Content content){
+    public static void post(final String apiKey, final Content content) {
 
-        try{
+        new AsyncTask<Void, Void, String>() {
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
 
-            // 1. URL
-            URL url = new URL("https://android.googleapis.com/gcm/send");
+                // 1. URL
+                URL url = new URL("https://android.googleapis.com/gcm/send");
 
-            // 2. Open connection
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                // 2. Open connection
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-            // 3. Specify POST method
-            conn.setRequestMethod("POST");
+                // 3. Specify POST method
+                conn.setRequestMethod("POST");
 
-            // 4. Set the headers
-            conn.setRequestProperty("Content-Type", "application/json");
-            conn.setRequestProperty("Authorization", "key="+apiKey);
+                // 4. Set the headers
+                conn.setRequestProperty("Content-Type", "application/json");
+                conn.setRequestProperty("Authorization", "key=" + apiKey);
 
-            conn.setDoOutput(true);
+                conn.setDoOutput(true);
 
-            // 5. Add JSON data into POST request body
+                // 5. Add JSON data into POST request body
 
-            //`5.1 Use Jackson object mapper to convert Contnet object into JSON
-            ObjectMapper mapper = new ObjectMapper();
 
-            // 5.2 Get connection output stream
-            DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+                //`5.1 Use Jackson object mapper to convert Contnet object into JSON
+                ObjectMapper mapper = new ObjectMapper();
 
-            // 5.3 Copy Content "JSON" into
-            mapper.writeValue(wr, content);
+                // 5.2 Get connection output stream
+                DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
 
-            // 5.4 Send the request
-            wr.flush();
+                // 5.3 Copy Content "JSON" into
+                mapper.writeValue(wr, content);
 
-            // 5.5 close
-            wr.close();
 
-            // 6. Get the response
-            int responseCode = conn.getResponseCode();
-            System.out.println("\nSending 'POST' request to URL : " + url);
-            System.out.println("Response Code : " + responseCode);
+                // 5.4 Send the request
+                wr.flush();
 
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(conn.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
+                // 5.5 close
+                wr.close();
+                // 6. Get the response
+                int responseCode = conn.getResponseCode();
+                Log.i("CONNECTION:", "\nSending 'POST' request to URL : " + url);
+                Log.i("CONNECTION:", "Response Code : " + responseCode);
 
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(conn.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                // 7. Print result
+                Log.i("RESPONSE:", response.toString());
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            in.close();
-
-            // 7. Print result
-            System.out.println(response.toString());
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            return null;
         }
+        }.execute(null, null, null);
     }
 
     public void onButtonClick(View view) {
