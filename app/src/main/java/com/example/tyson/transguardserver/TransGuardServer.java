1e3f553cc2731018e84b531f01905121431e2b39
Oatslinger <wmike.uetz@gmail.com>
2014-11-26 20:52:41 -0500

    #SWAGSWAG

--- app/src/main/java/com/example/tyson/transguardserver/TransGuardServer.java
+++ app/src/main/java/com/example/tyson/transguardserver/TransGuardServer.java
@@ -5,6 +5,15 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class TransGuardServer extends Activity {
 
@@ -14,6 +23,76 @@ public class TransGuardServer extends Activity {
         setContentView(R.layout.activity_trans_guard_server);
     }
 
+    public static void post(String apiKey, Content content){
+
+        try{
+
+            // 1. URL
+            URL url = new URL("https://android.googleapis.com/gcm/send");
+
+            // 2. Open connection
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            // 3. Specify POST method
+            conn.setRequestMethod("POST");
+
+            // 4. Set the headers
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Authorization", "key="+apiKey);
+
+            conn.setDoOutput(true);
+
+            // 5. Add JSON data into POST request body
+
+            //`5.1 Use Jackson object mapper to convert Contnet object into JSON
+            ObjectMapper mapper = new ObjectMapper();
+
+            // 5.2 Get connection output stream
+            DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+
+            // 5.3 Copy Content "JSON" into
+            mapper.writeValue(wr, content);
+
+            // 5.4 Send the request
+            wr.flush();
+
+            // 5.5 close
+            wr.close();
+
+            // 6. Get the response
+            int responseCode = conn.getResponseCode();
+            System.out.println("\nSending 'POST' request to URL : " + url);
+            System.out.println("Response Code : " + responseCode);
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // 7. Print result
+            System.out.println(response.toString());
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Content createContent(){
+
+        Content c = new Content();
+
+        c.addRegId("APA91bFqnQzp0z5IpXWdth1lagGQZw1PTbdBAD13c-UQ0T76BBYVsFrY96MA4SFduBW9RzDguLaad-7l4QWluQcP6zSoX1HSUaAzQYSmI93....");
+        c.createData("Test Title", "Test Message");
+
+        return c;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
